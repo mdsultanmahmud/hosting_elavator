33911b095f3a4fa1036501501879ae1e6db06acf
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-06-14 00:04:38 +0600

    latest news and exhibition section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner/Banner";
 import ElevatorMenu from "../../components/ElevatorMenu/ElevatorMenu";
+import LatestNews from "../../components/LatestNews/LatestNews";
 import Quote from "../../components/Quote/Quote";
 import Video from "../../components/Video/Video";
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <ElevatorMenu />
             <Video/>
+            <LatestNews/>
             <Quote />
         </div>
     );
